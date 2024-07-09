300474e35c30efbe094031cef6ae4d8de6087da8
Amashi Costa <amashinayonika.98@gmail.com>
2024-07-09 14:01:56 +0530

    Update the Branch with different Dynamic Values

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 function App() {
 
   const title = "This is My new Blog";
+  const likes = 50;
+  const person = {
+    name: "Amashi",
+    Age: 25
+  }
+  const link = "http://www.google.com";
 
   return (
     <div className="App">
       <div className="content">
         <h1> {title}</h1>
+        <p>Likes: {likes}</p>
+        <p>Name : {person.name}</p>
+        <p> {Math.random() * 100} </p>
+        <p> [1, 2, 3, 4.5, 5]</p>
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
